ddb4373d9fdc6cae1032f8839de64178ae86a71f
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-10 07:14:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,3 @@
-# Write your code below game_hash
 def game_hash
   {
     home: {
@@ -15,8 +14,7 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        },
-        {
+        }, {
           player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
@@ -26,8 +24,7 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        },
-        {
+        }, {
           player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
@@ -37,19 +34,17 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        },
-        {
+        }, {
           player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
           points: 26,
-          rebounds: 11,
+          rebounds: 12,
           assists: 6,
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        },
-        {
+        }, {
           player_name: "Jason Terry",
           number: 31,
           shoe: 15,
@@ -76,19 +71,17 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
+        }, {
+          player_name: "Bismak Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
           rebounds: 4,
           assists: 7,
-          steals: 22,
+          steals: 7,
           blocks: 15,
           slam_dunks: 10
-        },
-        {
+        }, {
           player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
@@ -98,8 +91,7 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5
-        },
-        {
+        }, {
           player_name: "Ben Gordon",
           number: 8,
           shoe: 15,
@@ -109,15 +101,14 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
+        }, {
+          player_name: "Brendan Haywood",
           number: 33,
           shoe: 15,
           points: 6,
           rebounds: 12,
           assists: 12,
-          steals: 7,
+          steals: 22,
           blocks: 5,
           slam_dunks: 12
         }
@@ -126,4 +117,18 @@ def game_hash
   }
 end
 
-# Write code here
+  
+def shoe_size(name)
+  player = find_the_player(name)
+  player.fetch(:shoe)
+end
+
+def num_points_scored(name)
+  player = find_the_player(name)
+  player.fetch(:points)
+end
+
+def team_colors(team_name)
+  team = find_the_team(team_name)
+  team.fetch(:colors)
+end
