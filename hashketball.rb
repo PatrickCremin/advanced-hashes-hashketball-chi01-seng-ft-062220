71e359823ad61a39d08095493ec696293372a00f
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-10 07:36:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -100,7 +100,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 0
       }, {
-        player_name: "Kemba Walker",
+        player_stats( Kemba Walker")
         
         number: 33,
         shoe: 15,
