4073b8547a2afb95131c5c27a89e59a6d9b043ae
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-10 07:33:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,7 +80,7 @@ def game_hash
         blocks: 15,
         slam_dunks: 10
       }, {
-        player_name: "DeSagna Diop",
+        player_name: DeSagna Diop,
         number: 2,
         shoe: 14,
         points: 24,
@@ -90,7 +90,7 @@ def game_hash
         blocks: 5,
         slam_dunks: 5
       }, {
-        player_name: "Ben Gordon",
+        player_name: Ben Gordon,
         number: 8,
         shoe: 15,
         points: 33,
@@ -100,7 +100,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 0
       }, {
-        player_name: "Kemba Walker",
+        player_name: Kemba Walker,
         number: 33,
         shoe: 15,
         points: 6,
